3868d5e054e3628d37d1db813e7010681e9f70e4
akshit <akshitkh.singh@gmail.com>
2022-07-19 16:28:50 +0530

    sol3 added

---
+++ Nodejs/sol3.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+var options = {
+    host: "www.google.com",
+    port: 80,
+    path: "/"
+};
+
+//non stream saving part
+var body = '';
+var res = http.request(options,(res)=>{
+    res.on('data',(data)=>{
+        body+=data;
+    })
+
+    res.on('end',(result)=>{
+        fs.writeFile('nobuff.txt',body,(err)=>{
+            if(err) return console.log(err);
+
+            console.log('file is written');
+        })
+    })
+})
+
+res.end();
+
+//this is the stream saving part
+var buffStream = fs.createWriteStream('buff.txt');
+var res1 = http.request(options,(res)=>{
+    res.on('data',(data)=>{
+        buffStream.write(data);
+    })
+
+    res.on('end',(result)=>{
+        buffStream.end();
+    })
+})
+
+
+
+res1.end();
